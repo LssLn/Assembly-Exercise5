d76cf58e3748026c5befa339a02f26a44c8a925a
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-01-20 00:08:58 +0100

    Add files via upload

---
+++ asm 17-09-2020.asm
@@ -0,0 +1,103 @@
+;# int elabora(char *st, int d)
+;# { int i,conta;
+;# conta=0;
+;# for(i=0;i<d;i++)
+;# if(st[i]<58) //if tutte lettere --> conta++
+;# conta++;
+;# return conta;
+;# }
+;# main() {
+;# char STR[16];
+;# int val;
+;# do{
+;# printf("Inserisci una stringa\n");
+;# scanf("%s",STR);
+;# val=elabora(ST,strlen(ST));
+;# if(val!=0)
+;# printf(" Valore= %d \n",val);
+;# }
+;# while (val!=0); //finisce quando il valore è 0 (tutte lettere)
+;# }
+
+.data 
+ST: .space 16
+stack: .space 32
+
+msg1: .asciiz "Inserire stringa \n"
+msg2: .asciiz "Valore : %d\n" ;# con val 1° arg msg2
+
+p1sys5: .space 8
+val: .space 8 ;# val, int aka 1° arg msg2
+
+p1sys3: .word 0 ;#fd null
+ind: .space 8
+dim: .word 16 ;# numbyte da leggere, <= ST 
+
+.code 
+;# inizializzazione stack
+daddi $sp,$0,stack
+daddi $sp,$sp,32
+
+daddi $s0,$0,0 ;# i=0
+do: ;# while val!=0, ma prima devo inserirlo. Non metto come sempre le condizioni slti e beq, ci penso dopo aver preso val
+
+    ;#printf msg1
+    daddi $t0,$0,msg1
+    sd $t0,p1sys5($0)
+    daddi r14,$0,p1sys5
+    syscall 5
+    ;# scanf %s, ST
+    daddi $t0, $0, ST
+    sd $t0, ind($0)
+    daddi r14,$0,p1sys3
+    syscall 3
+    ;# passaggio parametri
+    move $a1,r1         ;# $a1 = strlen(ST)
+    daddi $a0,$0,ST     ;# $a0 = ST
+
+    jal elabora
+    sd r1, val($0)  ;# return in val, 1° arg msg2
+    dadd $s4,$0,r1 ;# $s4 = val
+    ;# if(Val!=0) printf msg2
+    ;#ld $t0,val($0) ;# $t0 = val ;# uso invece $s4
+    beq $s4,$0, exit ;# se val== 0 esco dal programma. Se no continuo
+    ;#printf msg2
+    daddi $t0,$0,msg2
+    sd $t0,p1sys5($0)
+    daddi r14,$0,p1sys5
+    syscall 5 ;# alla fine prosegue con il do
+
+    j do    ;# se no continua e salta a do
+
+elabora: ;# $a0 = ST, $a1 = strlen
+    daddi $sp,$sp,-16 ;#i e conta
+    sd $s1,0($sp)   ;# i
+    sd $s2,8($sp)   ;# conta
+    daddi $s1,$0,0 ;# i=0
+    daddi $s2,$0,0 ;#conta=0
+
+for:
+    slt $t0,$s1,$a1 ;# $t0=0 quando $s1>=$a1, ovvero i >= strlen(=d)
+    beq $t0,$0,fine_for ;# fine for quando i>=strlen, ossia $t0=0
+    
+    ;# if(st[i]<58)
+    dadd $t0,$a0,$s1 ;# $t0 = &st[i] = st ($a0) + i ($s1)
+    lbu $t1,0($t0)  ;# $t1 = st[i]
+
+    slti $t0,$t1,58 ;# $t0 = 0 quando $t1 (st[i]) >= 58
+    beq $t0,$0, falso ;# salta l'incremento di conta, scorre for
+    daddi $s2,$s2,1 ;#conta ++ se $t0 !== 0, ovvero st[i] < 58 (numeri), poi va a falso
+falso: 
+    daddi $s1,$s1,1 ;# i++
+    j for
+fine_for:
+    move r1,$s2 ;# r1 = $s2 = conta
+    ld $s1,0($sp)
+    ld $s2,8($sp)
+    daddi $sp,$sp,16
+    jr $ra
+
+
+exit:
+    syscall 0
+
